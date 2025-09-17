a53729cc1e1d3a368c98bad4dc02405f674e4793
kitomc <kitomc.rd@gmail.com>
2025-09-17 12:36:58 -0400

    chore: upgrade Temi SDK from 1.121.0 to 1.131.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,6 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
 
-    // Temi SDK (ajustamos si tu versión difiere)
-    implementation("com.robotemi:sdk:1.121.0")
+    // Temi SDK (use a version published on Maven Central)
+    implementation("com.robotemi:sdk:1.131.4")
 }
