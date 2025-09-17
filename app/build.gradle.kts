01b6bdc3bd1f240f3fdfb99646dd72520b75aa21
kitomc <kitomc.rd@gmail.com>
2025-09-17 13:09:15 -0400

    chore: add Android test dependencies and configure test runner

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,6 +14,7 @@ android {
         versionCode = 1
         versionName = "1.0"
         vectorDrawables.useSupportLibrary = true
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
@@ -42,4 +43,9 @@ dependencies {
 
     // Temi SDK (use a version published on Maven Central)
     implementation("com.robotemi:sdk:1.131.4")
+
+    // Test dependencies
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 }
